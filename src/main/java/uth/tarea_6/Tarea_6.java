a5b4b7ecc800b854173ab02200af511342957610
kevinrodriguez100 <150084289+kevinrodriguez100@users.noreply.github.com>
2023-11-06 20:19:28 -0600

    primer commit para repositorio remoto

---
+++ src/main/java/uth/tarea_6/Tarea_6.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package uth.tarea_6;
+
+/**
+ *
+ * @author ksalo
+ */
+public class Tarea_6 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
